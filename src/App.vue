ea01c15d1eea2befe208ce67181d65728acdaa0e
Your Name <you@example.com>
2019-11-29 16:36:42 +0800

    123

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+*{
+  padding:0;
+  margin: 0;
+}
+#app,html,body{
+  width: 100%;
+  height: 100%;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper">
+
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  props:{
+
+  },
+  components:{
+
+  },
+  data(){
+    return {
+        list:[]
+    }
+  },
+  computed:{
+
+  },
+  methods:{
+
+  },
+  created(){
+
+  },
+  mounted(){
+      axios.get('https://baojia.chelun.com/v2-car-getMasterBrandList.html').then(res=>{
+        console.log(res);
+        
+      })
+  }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
